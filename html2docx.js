b1bc1fcc76b1dad515f08fba2f0b87bdfb9e6fe4
Ömer Kaptan <omer.kaptan@icloud.com>
2023-03-16 01:12:59 +0300

    printdocx func return fix

--- html2docx.js
+++ html2docx.js
@@ -1,31 +1,34 @@
 const fs = require("fs");
+const { promisify } = require("util");
 const HTMLtoDOCX = require("html-to-docx");
+const writeFileAsync = promisify(fs.writeFile);
 const printDocx = async (html, filename, options) => {
   const filePath = filename || "";
   const htmlString = html || "testing";
-  const optionsObj = options || {}
-  const fileBuffer = await HTMLtoDOCX(htmlString, null, optionsObj);
-  fs.writeFile(filePath, fileBuffer, (error) => {
-    if (error) {
-      return error;
-    } else {
-      // TODO: Başarı Durumunu return ederken Arayüzdeki Seçime Göre Ele Al <buffer|filepath>
-    }
-  });
+  const optionsObj = options || {};
+  const fileBuffer = await HTMLtoDOCX(htmlString, null, optionsObj)
+  writeFileAsync(filePath, fileBuffer);
+  return fileBuffer;
 };
 
 module.exports = function (RED) {
   function html2Docx(config) {
     RED.nodes.createNode(this, config);
     const node = this;
-    let {Output} = config
+    let { output } = config;
     node.on("input", async (msg, send, done) => {
       if (msg.hasOwnProperty("payload")) {
         try {
-          const docx = await printDocx(msg.payload, msg.filename, msg.options);
-          msg.payload = docx || "1";
-          send(msg);
-          done();
+          let docx = await printDocx(msg.payload, msg.filename, msg.options)
+            .then((fileBuffer) => {
+              console.log("File buffer:", fileBuffer);
+              msg.payload = docx || "1";
+              send(msg);
+              done();
+            })
+            .catch((error) => {
+              done(error);
+            });
         } catch (error) {
           done(error);
         }
